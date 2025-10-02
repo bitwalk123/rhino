360a2319a0614ecf940bc04076ea538537651a66
Fuhito Suguri <bitwalk@gmail.com>
2025-10-02 10:11:08 +0900

    update

--- monitor_plot.py
+++ monitor_plot.py
@@ -2,27 +2,23 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# monitor.csvの読み込み
+# monitor.csv の読み込み
 dir_log = "./logs/"
 name_log = "monitor.csv"
 df = pd.read_csv(os.path.join(dir_log, name_log), skiprows=[0])
-print(df)
+print(df.head())
 
 # 報酬のプロット
-#x = range(len(df['r']))
-#y = df['r'].astype(float)
-#plt.plot(x, y)
 plt.plot(df["r"])
 plt.xlabel('episode')
 plt.ylabel('reward')
+plt.grid()
 plt.tight_layout()
 plt.show()
 
 """
 # エピソード長のプロット
-x = range(len(df['l']))
-y = df['l'].astype(float)
-plt.plot(x, y)
+plt.plot(df["r"])
 plt.xlabel('episode')
 plt.ylabel('episode len')
 plt.show()
