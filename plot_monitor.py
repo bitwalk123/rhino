3c60501f0a806052cf4cd16c650c5ea23e8044ea
Fuhito Suguri <bitwalk@gmail.com>
2025-10-02 14:48:36 +0900

    update

---
+++ plot_monitor.py
@@ -0,0 +1,18 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    # monitor.csv の読み込み
+    dir_log = "./logs"
+    name_log = "monitor.csv"
+    # 最初の行の読み込みを除外
+    df = pd.read_csv(os.path.join(dir_log, name_log), skiprows=[0])
+
+    # 報酬のプロット
+    plt.plot(df["r"])
+    plt.xlabel("episode")
+    plt.ylabel("reward")
+    plt.grid()
+    plt.tight_layout()
+    plt.show()
