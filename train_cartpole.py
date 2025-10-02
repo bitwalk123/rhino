c17803067ea95e6ccf2eadf5d7b50d0ccea6ba60
Fuhito Suguri <bitwalk@gmail.com>
2025-10-02 09:21:40 +0900

    update

--- train_cartpole.py
+++ train_cartpole.py
@@ -1,14 +1,22 @@
+import os
+
 import gymnasium as gym
 from stable_baselines3 import PPO
+from stable_baselines3.common.monitor import Monitor
 
 if __name__ == "__main__":
+    # ログフォルダの準備
+    dir_log = './logs/'
+    os.makedirs(dir_log, exist_ok=True)
+
+    # 学習環境の準備
     env = gym.make("CartPole-v1", render_mode="human")
+    env = Monitor(env, dir_log)  # Monitorの利用
 
     # モデルの準備
     model = PPO('MlpPolicy', env, verbose=True)
 
     # 学習の実行
-    #model.learn(total_timesteps=128000)
     model.learn(total_timesteps=50000)
 
     # 推論の実行
@@ -19,7 +27,6 @@ if __name__ == "__main__":
     total_reward = 0
 
     while not episode_over:
-        #action = env.action_space.sample()  # Random action for now - real agents will be smarter!
         action, _ = model.predict(obs, deterministic=True)
 
         obs, reward, terminated, truncated, info = env.step(action)
