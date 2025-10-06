7738b66b6d258db088f1e86f68088135641c7724
Fuhito Suguri <bitwalk@gmail.com>
2025-10-06 17:46:22 +0900

    update

--- modules/agent.py
+++ modules/agent.py
@@ -19,7 +19,7 @@ class PPOAgent(QObject):
         super().__init__()
         self.res = res
         self._stopping = False
-        self.total_timesteps = 262144
+        self.total_timesteps = 524288
 
     def get_env(self, file: str, code: str) -> TradingEnv:
         # Excel ファイルをフルパスに
