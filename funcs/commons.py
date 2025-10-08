9f4e4b89dc35f3b7e24e97a951d7febbe9de2e7e
Fuhito Suguri <bitwalk@gmail.com>
2025-10-08 16:23:39 +0900

    update

--- funcs/commons.py
+++ funcs/commons.py
@@ -39,3 +39,10 @@ def get_name_15min_chart_now(code: str) -> str:
     month = dt_now.month
     day = dt_now.day
     return f"{year:4d}/{month:02d}{day:02d}_15min_chart_{code}.png"
+
+
+def get_name_15min_chart_usd(code: str, dt: datetime.datetime) -> str:
+    year = dt.year
+    month = dt.month
+    day = dt.day
+    return f"{year:4d}/{month:02d}{day:02d}_15min_chart_{code}_usd.png"
