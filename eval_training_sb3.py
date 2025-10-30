9ac2ec20de0a89e2af6ca2f09769cc0f308316c5
Fuhito Suguri <bitwalk@gmail.com>
2025-10-30 13:12:31 +0900

    update

--- eval_training_sb3.py
+++ eval_training_sb3.py
@@ -12,9 +12,8 @@ if __name__ == "__main__":
 
     # 学習用データ
     code = "7011"
-    # list_file = ["ticks_20250819.xlsx"]
+    list_file = ["ticks_20250819.xlsx"]
     # list_file = sorted(os.listdir(res.dir_collection))
-    list_file = sorted(os.listdir(res.dir_collection)[:5])
     flag_new_model = True
     for file in list_file:
         print(f"学習するティックデータ : {file}")
