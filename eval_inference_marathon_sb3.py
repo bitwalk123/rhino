116c80125b55fc2d3c4ec681efb99e9f70ce8e0b
Fuhito Suguri <bitwalk@gmail.com>
2025-11-16 06:46:08 +0900

    update

--- eval_inference_marathon_sb3.py
+++ eval_inference_marathon_sb3.py
@@ -66,6 +66,6 @@ if __name__ == "__main__":
     df_result.to_csv(path_result)
 
     t_delta = t_end - t_start
-    print(f"計測時間 :\t\t{t_delta * 1_000:,.6f} msec")
+    print(f"計測時間 :\t\t{t_delta * 1_000_000:,.1f} sec")
     print(f"総ティック量 :\t\t{n_tick:,d} tick")
     print(f"時間 / 1 ティック :\t{t_delta / n_tick * 1_000:.6f} msec")
